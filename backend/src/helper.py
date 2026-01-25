79891c8bfffdc90a598a77d3f66100e1b791becd
Wassim Hajjar <wassimhajjar926@gmail.com>
2026-01-25 11:58:27 +0200

    helper commit

--- backend/src/helper.py
+++ backend/src/helper.py
@@ -13,6 +13,7 @@ from langchain_classic.chains.combine_documents import create_stuff_documents_ch
 from langchain_core.prompts import ChatPromptTemplate,MessagesPlaceholder
 from langchain_core.runnables.history import RunnableWithMessageHistory
 from src.prompt import *
+from sentence_transformers import SentenceTransformer
 
 
 
@@ -57,7 +58,9 @@ def download_hugging_face_embeddings():
     """
     Download and return the HuggingFace embeddings model
     """
-    model_name="sentence-transformers/all-MiniLM-L6-v2"
+    # model_name="sentence-transformers/all-MiniLM-L6-v2"
+    model_name="all-MiniLM-L6-v2"
+    # model_name=SentenceTransformer("all-MiniLM-L6-v2")
     embeddings= HuggingFaceEmbeddings(
         model_name=model_name
     )
